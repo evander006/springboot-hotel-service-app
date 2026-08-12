04b6c5352facd7604e538006ae65f9dab4354fe6
Anufrieva Eva <anufrieva.ev@intechs.by>
2026-08-12 16:48:48 +0300

    first commit

---
+++ src/main/java/evaanufr/dev/springboothotelserviceapp/SpringbootHotelServiceAppApplication.java
@@ -0,0 +1,13 @@
+package evaanufr.dev.springboothotelserviceapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootHotelServiceAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootHotelServiceAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/evaanufr/dev/springboothotelserviceapp/SpringbootHotelServiceAppApplicationTests.java
@@ -0,0 +1,13 @@
+package evaanufr.dev.springboothotelserviceapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootHotelServiceAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
